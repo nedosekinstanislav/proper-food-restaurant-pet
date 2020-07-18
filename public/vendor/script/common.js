6ebdb6c2990e5a22f133cef746741609f6b8a9f8
NedosekinStas <stasnedosekin@mail.ru>
2020-07-18 21:51:19 +0300

    error script commonjs

--- public/vendor/script/common.js
+++ public/vendor/script/common.js
@@ -4,33 +4,49 @@
 
 function locStr() {
 
-    let som = window.localStorage.getItem('test');
+    let som = localStorage.getItem('test');
 
     som = som * 1 + 1; 
-    window.localStorage.setItem('test', som);
-    
-    console.log(som);
+    localStorage.setItem('test', som);
 };
 
 function addToCart(id) {
-    let key = 'product ' + id
-    let productId = window.localStorage.getItem(key);
+    let key = 'product_ ' + id
+    let productId = localStorage.getItem(key);
     
     productId = productId * 1 + 1;
-    window.localStorage.setItem(key, productId)
-    console.log('add product with id: ' + productId);
+    localStorage.setItem(key, productId)
+
+    updateOrders();
 };
 
-function getNumber(params) {
+function updateOrders() {
+    const orders = getOrders();
+    $('.orders-input').val(orders);
+}
+
+function getNumber() {
     const cnt = 0;
-    for (let index = 0; index < window.localStorage.length; index++) {
-        const key = window.localStorage.key(index);
-        const value = window.localStorage.getItem(key);
+    for (let index = 0; index < localStorage.length; index++) {
+        const key = localStorage.key(index);
+        const value = localStorage.getItem(key);
 
         if (key.indexOf('product_') == 0){
             cnt = cnt + value;
         };
     };
+    return cnt;
 };
 
-console.log(getNumber());
+function getOrders() {
+    const orders = '';
+    for (let index = 0; index < localStorage.length; index++) {
+        const key = localStorage.key(index);
+        const value = localStorage.getItem(key);
+
+        if (key.indexOf('product_') == 0){
+           orders = orders + key + '=' + value + ',';
+        };
+        return orders;
+    };
+};
